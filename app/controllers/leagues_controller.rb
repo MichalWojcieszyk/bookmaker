3f687ffc1795cce1154b4ca257fa6f4d9a1ae71d
MichalWojcieszyk <mwojcieszyk@gmail.com>
2016-03-05 16:36:21 +0100

    add models associations

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,3 +1,40 @@
 class LeaguesController < ApplicationController
+  expose(:league)
+  expose(:leagues)
 
+	def index
+	end
+
+	def create
+		if league.save
+			redirect_to league_path(league)
+		else
+			render :new
+		end
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def update
+		if league.save
+			redirect_to league_path(league)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		league.destroy
+		redirect_to leagues_path
+	end
+
+	private
+
+	def league_params
+		params.require(league).permit(:name, :teams, :season)
+	end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,4 @@
 class Match < ActiveRecord::Base
+	belongs_to :league
+	has_many :types, dependent: :destroy
 end
